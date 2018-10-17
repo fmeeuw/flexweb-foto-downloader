dde3bf4053dd1785f45aa885fcfbe39a6ad182f7
fmeeuw <frank@frankit.io>
2018-10-17 23:17:27 +0200

    Added depenency on exiftool, to update the medata of the fotos, such that the date is correct. But also add description, keywords and geolocation.

--- flexfotos.sh
+++ flexfotos.sh
@@ -2,22 +2,33 @@
 
 # FlexFotos.sh for fetching fotos from flexweb app like https://mijn.kmnkindenco.nl./
 # Requires curl for performing http requests and jq for parsing json.
+
+# This application requires cUrl for retrieving the images, jq for parsing json and exiftool for adding exif metadata.
+function checkDependencies() {
+    for program in curl jq exiftool
+    do
+        hash "${program}" 2>/dev/null || { echo >&2 "${program} is required for this script, but it's not installed."; exit 1; }
+    done
+}
+
 function checkArgs() {
     if [ -z "${username}" ]
     then
-        echo "Missing or empty username."
+        echo >&2 "Missing or empty username."
         display_usage
     fi
     if [ -z "${password}" ]
     then
-        echo "Missing or empty password."
+        echo >&2 "Missing or empty password."
         display_usage
     fi
 }
 function display_usage() {
-    echo "usage: flexfotos -u username -p password -t [target_dir] -s [start_year] -e [end_year]"
+    echo "usage: flexfotos -u username -p password -t [target_dir] -s [start_year] -e [end_year] -k [keyword1, keyword2, keyword3] -l [latitude] -o [longitude]"
+    echo "The keywords, latitude and longitude are written to the IPTC:Keywords, XMP:Latitude and XMP:longitude metadata of all fotos."
     exit 1
 }
+
 
 
 script_dir=$(cd "$(dirname "$0")" && pwd -P)
@@ -25,9 +36,14 @@ cookie_file="${script_dir}/session.cookie"
 target_dir=./target
 start_year=2014
 end_year=2020
+keywords=""
+gps_latitude=""
+gps_longitude=""
+
+checkDependencies
 
 # Parse and get script arguments
-while getopts ":u:p:t:s:e:" opt; do
+while getopts ":u:p:t:s:e:k:l:o:" opt; do
   case "${opt}" in
     u)
        username=$OPTARG
@@ -44,20 +60,27 @@ while getopts ":u:p:t:s:e:" opt; do
     e)
        end_year=$OPTARG
        ;;
+    k)
+       keywords=$OPTARG
+       ;;
+    l)
+       gps_latitude=$OPTARG
+       ;;
+    o)
+       gps_longitude=$OPTARG
+       ;;
     \?)
-      echo "Invalid option: -$OPTARG" >&2
+      echo >&2 "Invalid option: -$OPTARG"
       exit 1
       ;;
     :)
-      echo "Option -$OPTARG requires an argument." >&2
+      echo >&2 "Option -$OPTARG requires an argument."
       exit 1
       ;;
   esac
 done
 
 checkArgs
-
-
 
 
 # Remove old session cookie if exists
@@ -70,32 +93,49 @@ curl 'https://mijn.kmnkindenco.nl/login/login' \
  --cookie-jar ${cookie_file} \
  --data-urlencode "username=${username}" \
  --data-urlencode "password=${password}" \
- --data-urlencode "role=7" \
+ --data-urlencode "role=7"
 
 echo "Navigating to ${target_dir}, creating directories if needed."
 mkdir -p ${target_dir}
 cd ${target_dir}
 for ((year=start_year; year < end_year; year++)); do
     for month in {1..12}; do
-        # Set working directory to current year/month.
-        wd="${year}-${month}"
-        mkdir -p ${wd}
-        cd ${wd}
 
+        # Retrieve the ids of the fotos for the specific year and month.
+        echo "Retrieving fotos for ${year}-${month}."
+        foto_ids=$( \
+            curl 'https://mijn.kmnkindenco.nl/ouder/fotoalbum/standaardalbum' \
+             --cookie ${cookie_file} \
+             --data-urlencode "year=${year}" \
+             --data-urlencode "month=${month}" | \
+             jq -r ".FOTOS | .[]" \
+             )
 
-        # Retrieve the fotos for the specific year and month, and also download them.
-        echo "Retrieving fotos for ${year}-${month}, storing them at ${target_dir}/${wd}."
-        curl 'https://mijn.kmnkindenco.nl/ouder/fotoalbum/standaardalbum' \
-         --cookie ${cookie_file} \
-         --data-urlencode "year=${year}" \
-         --data-urlencode "month=${month}" | \
-         jq ".FOTOS | .[]" | \
-         xargs -L1 -I'{}' curl 'https://mijn.kmnkindenco.nl/ouder/media/download/media/{}' \
-         --cookie ${cookie_file} \
-         -o "${year}-${month}-{}.jpg"
+         for foto_id in ${foto_ids}; do
+            echo "Downloading foto ${foto_id}."
+            foto_file="${foto_id}.jpg"
+            curl "https://mijn.kmnkindenco.nl/ouder/media/download/media/${foto_id}" \
+             --cookie ${cookie_file} \
+             -o "${foto_file}"
 
-         cd ..
+            echo "Retrieving metadata...."
+            metadata=$( \
+                curl "https://mijn.kmnkindenco.nl/ouder/fotoalbum/fotometa" \
+                 --cookie ${cookie_file} \
+                 --data-urlencode "id=${foto_id}" | \
+                 jq ".[0] | {MEDIA_DAG, MEDIA_BESCHRIJVING}" \
+                 )
+            image_description=$(echo ${metadata} | jq -r ".MEDIA_BESCHRIJVING")
+            image_date=$(echo ${metadata} | jq -r ".MEDIA_DAG")
+
+            echo "Updating metadata setting date to ${image_date}, description to ${image_description} and adding keywords ${keywords} and gps coordinates ${gps_latitude} / ${gps_longitude}."
+            exiftool -overwrite_original -sep ", " -keywords+="${keywords}" -imagedescription="${image_description}" \
+             -alldates="${image_date}" -xmp:gpslatitude="${gps_latitude}" -xmp:gpslongitude="${gps_longitude}" ${foto_file}
+         done
     done
 done
 
-
+ # Rename all fotos so they start with the date part.
+ exiftool "-FileName<CreateDate" -d "%Y-%m-%d_%%f.%%e" ${target_dir}
+ # Organize files in a directory hiearchy, a directory per year and per month.
+ exiftool "-directory<CreateDate" -d "%Y/%Y-%m" ${target_dir}
